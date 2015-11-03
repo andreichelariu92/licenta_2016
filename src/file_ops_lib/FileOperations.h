a28f38939715a5d255f489dd79af2bcd6a5fc139
Andrei Chelariu <andreichelariu92@yahoo.com>
2015-11-03 21:35:39 +0200

    run spell check on file

--- src/file_ops_lib/FileOperations.h
+++ src/file_ops_lib/FileOperations.h
@@ -6,4 +6,5 @@
 
 std::vector<char> readData(const boost::filesystem::path& path, unsigned int position, unsigned int size);
 void overWriteData(const boost::filesystem::path& path, unsigned int position, const std::vector<char>& buffer);
+
 #endif // FILEOPERATIONS_H
