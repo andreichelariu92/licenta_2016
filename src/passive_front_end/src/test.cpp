eb056d69d8e15da6a7565c95e92eaad98e06a502
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-03-02 23:22:47 +0200

    Test getAllDirectories

--- src/passive_front_end/src/test.cpp
+++ src/passive_front_end/src/test.cpp
@@ -75,50 +75,11 @@ int main()
         }
     }
     */
-    /*
-    Directory d("/home/andrei");
-    vector<string> files = d.regularFiles();
-    for (string file : files)
-    {
-        std::cout << file << "\n";
-    }
-    std::this_thread::sleep_for (std::chrono::seconds(10)); 
-    vector<Directory> dirs = d.subDirectories();
+    
+    vector<Directory> dirs = getAllDirectories("/home/andrei/windows/Andrei/Facultate");
     for (Directory& dir : dirs)
     {
-        std::cout << dir.path() << "\n";
-    }
-
-    std::cout << isRegularFile("/home/andrei/main.cpp") << "\n";
-    std::cout << isDirectory("/home/andrei") << "\n";
-    */
-    InotifyInstance ii;
-    Directory d("/home/andrei/windows");
-    vector<Directory> subDirs = d.subDirectories();
-    vector<int> wds;
-    for (Directory& subDir : subDirs)
-    {
-        //cout << subDir.path() << "\n";
-        int wd = ii.addToWatch(subDir.path(),
-                      IN_CREATE | IN_DELETE | IN_MODIFY);
-        wds.push_back(wd);
-    }
-    vector<InotifyEvent> events = ii.readEvents(10000);
-    for (InotifyEvent& event : events)
-    {
-        cout << event.path;
-        if (event.mask & IN_DELETE)
-        {
-            cout << " was deleted\n";
-        }
-        else if (event.mask & IN_CREATE)
-        {
-            cout << " was created\n";
-        }
-        else if (event.mask & IN_MODIFY)
-        {
-            cout << " was modified\n";
-        }
+        cout << dir.path() << "\n";
     }
     return 0;
 }
