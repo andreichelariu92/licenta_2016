1b433195686ed017e84e58f8f2f0123a2b62c611
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-04-16 11:05:44 +0300

    Something changed in Logger module, but I don't know what :D

---
+++ src/util/src/Logger.cpp
@@ -0,0 +1,52 @@
+#include"Logger.h"
+Logger::Logger(std::string fileName)
+   :m_ofs(fileName, std::ios_base::out),
+    m_mutex()
+{
+}
+
+Logger& Logger::getInstance(std::string filename)
+{
+    static Logger instance(filename);
+    return instance;
+}
+Logger& Logger::operator <<(Logger::Prio data)
+{
+        switch(data)
+        {
+        case trace:
+            m_ofs<<"TRC  ";
+            break;
+        case debug:
+            m_ofs<<"DBG  ";
+            break;
+        case error:
+            m_ofs<<"ERR  ";
+            break;
+        case exception:
+            m_ofs<<"EXC  ";
+            break;
+        default:
+            break;
+        }
+        return *this;
+ }
+Logger& Logger::operator <<(Information info)
+{
+    info.threadId=std::this_thread::get_id();
+    std::time_t t= std::time(NULL);
+    //get hh:mm:ss from t
+    info.time=std::string( std::ctime( &t ), 11, 8);
+
+    {//enter critical section
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    m_ofs<<info.filename<<"  ";
+    m_ofs<<info.lineNumber<<"  ";
+    m_ofs<<info.threadId<<"  ";
+    m_ofs<<info.time<<"  ";
+
+    }//exit critical section
+
+    return *this;
+}
---
+++ src/util/src/Logger.h
@@ -0,0 +1,68 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include<mutex>
+#include<fstream>
+#include<thread>
+#include<string>
+#include<ctime>
+struct Information
+{
+    unsigned int lineNumber;
+    std::thread::id threadId;
+    std::string filename;
+    std::string time;
+
+    Information(unsigned int ln, std::string file)
+        :lineNumber(ln), threadId(0), filename(file), time()
+    {}
+
+};
+#define INFO Information(__LINE__, __FILE__)
+
+class Logger
+{
+private:
+    std::ofstream m_ofs;
+    std::mutex m_mutex;
+
+    //suppress creation and copy
+    //of instances for the singleton pattern
+    Logger(std::string fileName);
+    Logger& operator=(const Logger& source)=delete;
+    Logger(const Logger& source)=delete;
+public:
+    enum Prio
+    {
+        trace=0,
+        debug=1,
+        error=2,
+        exception=3
+    };
+
+    template<typename T>
+    Logger& operator<<(T data)
+    {
+        {//enter critical section
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_ofs<<data;
+        }//exit critical section
+        return *this;
+    }
+
+    Logger& operator<<(Prio data);
+    Logger& operator<<(Information info);
+    Logger& operator<<(const char* s)
+    {
+        {//enter critical section
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_ofs<<s;
+        }//exit critical section
+        return *this;
+    }
+
+
+    static Logger& getInstance(std::string filename="logfile");
+};
+#define LOG Logger::getInstance()
+#endif
