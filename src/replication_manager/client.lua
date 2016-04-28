2785f6fbd9b97ccfe31855a97f21fc7a9ec19f2c
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-04-29 00:02:14 +0300

    Make the function that receives events from pfe a coroutine

--- src/replication_manager/client.lua
+++ src/replication_manager/client.lua
@@ -4,8 +4,6 @@ socket = require("socket")
 json = require("json")
 socket.unix = require("socket.unix")
 
-c = assert(socket.unix())
-
 listOfFileEvents = {}
 
 function getFileEvent(jsonMessage)
@@ -25,22 +23,52 @@ function deserializeEvents(message)
     string.gsub(message, pattern, getFileEvent)
 end
 
-assert(c:connect("./file.txt"))
-line, err = c:receive()
-local count = 0
-while not err  and count < 3 do
-    print(line)
-    getFileEvent(line)
-    message = string.format("count = %d\n", count)
-    print(message)
-    c:send(message)
-    count = count + 1
-    line, err = c:receive()
+function getFileEvents(path, timeout)
+    --set default parameters
+    if not path then
+        path = "../passive_front_end/file.txt";
+    end
+    if not timeout then
+        timeout = 10
+    end
+    --create a unix socket and set
+    --it a timeout value
+    local clientSocket = assert(socket.unix())
+    assert(clientSocket:connect(path))
+    print("Client has connected\n")
+    while true do
+        --clear the list (remove the events
+        --from the previous iteration)
+        listOfFileEvents = {}
+        clientSocket:settimeout(timeout, 'b')
+        local line, err = clientSocket:receive()
+        print("line = ", line)
+        if not err then
+            deserializeEvents(line)
+            clientSocket:send("ack")
+        else
+            print("error = ", err)
+        end
+        coroutine.yield(listOfFileEvents)
+    end
+    clientSocket:close()
 end
-c:close()
 
-for k, fe in pairs(listOfFileEvents) do
-    print(fe.absolutePath)
-    print(fe.fileType)
-    print(fe.eventType)
+function consumer()
+    co_eventGenerator = coroutine.create(getFileEvents)
+    local count = 0
+    while count < 10 do
+        local state, fileEvents = coroutine.resume(co_eventGenerator)
+        if state then
+            print("count = ", count)
+            for k, v in pairs(fileEvents) do
+                print(v.absolutePath, v.fileEvent, v.fileType)
+                print("\n")
+            end
+        end
+
+        count  = count + 1
+    end
 end
+
+consumer()
