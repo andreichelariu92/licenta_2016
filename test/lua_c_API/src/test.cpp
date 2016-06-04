09ad395fc989664e5b7e68b8208ecfbf47c4d718
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-05 00:32:16 +0300

    Export C functions that test the latest modifications

--- test/lua_c_API/src/test.cpp
+++ test/lua_c_API/src/test.cpp
@@ -10,20 +10,76 @@ extern "C" {
 
 #include "LuaWrapper.h"
 
-int customPrint(lua_State* L)
-{
-    const char* message = lua_tostring(L, 1);
-    printf("from managed side %s\n", message);
-    return 0;
-}
-extern "C" {
-int luaopen_testLib(lua_State *L)
-{
-    LuaInterpreter li(L);
+using std::vector;
 
-    std::vector<LuaCFunction> functions;
-    functions.emplace_back(customPrint, "customPrint");
-    li.createFunctionTable("testLib", functions);
+struct Adder
+{
+    int state;
+    
+    Adder(int argState)
+        :state(argState)
+    {}
+
+    int getState()
+    {
+        return state;
+    }
+
+    void addState(int s)
+    {
+        state += s;
+    }
+};
+
+static Adder g_adder(0);
+
+int getAdder(lua_State* L)
+{
+    int state = g_adder.getState();
+
+    lua_pushnumber(L, state);
+
     return 1;
 }
+
+int add(lua_State* L)
+{
+    LuaInterpreter lua(L);
+    try
+    {
+        double number = lua.getNumber(1);
+        g_adder.addState(number);
+        return 0;
+    }
+    catch (std::exception& e)
+    {
+        lua_pushstring(L, e.what());
+        return 1;
+    }
+}
+
+int getMessages(lua_State* L)
+{
+    vector<TestMessage> messages;
+    messages.emplace_back("id1", "Hello");
+    messages.emplace_back("id2", "world");
+
+    LuaInterpreter lua(L);
+    lua.pushMessages(messages);
+
+    return 1;
+}
+
+extern "C" {
+    int luaopen_testLib(lua_State *L)
+    {
+        LuaInterpreter li(L);
+
+        std::vector<LuaCFunction> functions;
+        functions.emplace_back(getAdder, "get");
+        functions.emplace_back(add, "add");
+        functions.emplace_back(getMessages, "getMessages");
+        li.createFunctionTable("testLib", functions);
+        return 0;
+    }
 }
