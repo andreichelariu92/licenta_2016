5ce362329884d6a53ab8af29530ac5cbacc46803
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-05 00:33:05 +0300

    Test the latest changes from lua

--- test/lua_c_API/test.lua
+++ test/lua_c_API/test.lua
@@ -1,3 +1,11 @@
 print("Hello world")
 require"testLib"
-testLib.customPrint(" from lua")
+print(testLib.get())
+testLib.add(5)
+print(testLib.get())
+
+messages = testLib.getMessages()
+for _, message in pairs(messages) do
+    print(message.id, message.buffer)
+    print("\n")
+end
