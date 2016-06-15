730c97ad5be8e71941adeded6074cf2947a3dd9b
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-15 09:20:02 +0300

    Test the deinit function

--- src/broadcast_mechanism/test.lua
+++ src/broadcast_mechanism/test.lua
@@ -14,3 +14,10 @@ for messageIdx, message in pairs(messages) do
     print(message.id)
     print(message.buffer)
 end
+
+messages = bcast.deinit()
+for messageIdx, message in pairs(messages) do
+    print(messageIdx)
+    print(message.id)
+    print(message.buffer)
+end
