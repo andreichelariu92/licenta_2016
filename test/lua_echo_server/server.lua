d47b06325212f4d2500dbdc8fc1d0af884e41c9d
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-16 00:17:09 +0300

    Test latest changes

--- test/lua_echo_server/server.lua
+++ test/lua_echo_server/server.lua
@@ -1,11 +1,14 @@
 require("broadcast_mechanism")
 bcast.init(12345)
-local expectedClocks = 10
-local start = os.clock()
-while os.clock() - start < expectedClocks do
-end 
+function timeout(clocks)
+    local start = os.clock()
+    while os.clock() - start < clocks do
+        --wait
+    end
+end
 
+timeout(10)
 bcast.addConnection("127.0.0.1", 54321, "connection2")
 bcast.sendToAll("buffer", "id")
-
+timeout(10)
 bcast.deinit()
