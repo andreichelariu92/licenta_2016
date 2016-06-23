9e3d7943ddfc0c85c56f171f12f684fa08522ba5
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-23 16:48:26 +0300

    Process events received from the network

--- src/replication_manager/test.lua
+++ src/replication_manager/test.lua
@@ -1,7 +1,7 @@
 require("clients.broadcast_mechanism_client")
 require("clients.passive_front_end_client")
 require("events.event_processor")
-
+local json = require("json")
 function sleep(seconds)
     local start = os.clock()
     while os.clock() - start < seconds do
@@ -13,7 +13,6 @@ bcast_client.init(12345)
 event_processor.init("/home/andrei/test")
 
 sleep(5)
---bcast_client.addConnection("127.0.0.1", 54321, "connection1")
 co_fileEvents = coroutine.create(pfe_client.getFileEvents)
 local count = 0
 while count < 10 do
@@ -21,6 +20,15 @@ while count < 10 do
     if state then
         for k, event in pairs(fileEvents) do
             event_processor.processLocalEvent(event)
+        end
+    end
+
+    local messages = bcast_client.receive(1000)
+    for _, message in pairs(messages) do
+        print("Network event ", message.buffer)
+        local netEvent = json.decode(message.buffer)
+        if netEvent then
+            event_processor.processNetworkEvent(netEvent)
         end
     end
     count = count +1
