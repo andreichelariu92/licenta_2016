a36f44b6e10861955d6959e18376769bfc4ad096
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-06-23 16:46:45 +0300

    Add functions for file prefixes

--- src/replication_manager/file_operations.lua
+++ src/replication_manager/file_operations.lua
@@ -2,6 +2,17 @@
 --that can be used by the rest of the application
 local file_operations = {}
 
+--Module variable that will hold the prefix added to
+--the file or to the directory that will be locked.
+--See lock files on web for more information.
+local lockPrefix = ""
+
+--Function that initialized the variables needed by
+--this module
+function file_operations.init(argLockPrefix)
+    lockPrefix = argLockPrefix
+end
+
 function file_operations.createFile(path)
     local command = "touch " .. path
     print(command)
@@ -75,14 +86,32 @@ function file_operations.relativePath(absPath, rootDir)
 end
 
 --Function that returns the file name
---from the given path
+--from the given path and the parent dir
 function file_operations.getFileName(path)
     local parentDir = string.match(path, ".*/")
     if parentDir then
         local fileName = string.gsub(path, parentDir, "")
-        return fileName
+        return fileName, parentDir
     else
         return nil
+    end
+end
+
+--Function that returns true if the file or directory
+--at the given path has its name starting with the
+--specified prefix.
+function file_operations.hasPrefix(path, prefix)
+    local fileName = file_operations.getFileName(path)
+    if fileName then
+        --true specifies that prefix is not regex
+        local startIdx = string.find(fileName, prefix, 1, true)
+        if startIdx == 1 then
+            return true
+        else
+            return false
+        end
+    else
+        return false
     end
 end
 
@@ -91,4 +120,87 @@ end
 function file_operations.absolutePath(relPath, rootDir)
     return (rootDir .. relPath)
 end
+
+--Function that adds the spefied prefix to the path and then
+--returns the new path
+function file_operations.addPrefix(path, prefix)
+    local fileName, parentDir = file_operations.getFileName(path)
+    if fileName and parentDir then
+        fileName = prefix .. fileName
+        return (parentDir .. fileName)
+    else
+        return nil
+    end
+end
+
+--Function that removes the specified prefix from the given
+--path. If the file at the specified path does not have the
+--prefix, the function returns nil.
+function file_operations.removePrefix(path, prefix)
+    if not file_operations.hasPrefix(path, prefix) then
+        return nil
+    end
+    
+    local fileName, parentDir = file_operations.getFileName(path)
+    if fileName and parentDir then
+        --replace the first occurance of prefix with
+        --the empty string
+        fileName = string.gsub(fileName, prefix, "", 1)
+        return (parentDir .. fileName)
+    else
+        return nil
+    end
+end
+
+--Local function that creates the lock path for a file or
+--directory based on the current path.
+--For example: /home/andrei/file -> /home/andrei/~file
+local function getLockPath(path)
+    return file_operations.addPrefix(path, lockPrefix)
+end
+
+--Function that creates a lock file for the file at the given
+--path.
+function file_operations.lockFile(path)
+    local lockPath = getLockPath(path)
+    if lockPath then
+        return file_operations.createFile(lockPath)
+    else
+        return false
+    end
+end
+
+--Function that removes a lock file for the file at the given
+--path.
+function file_operations.unlockFile(path)
+    local lockPath = getLockPath(path)
+    if lockPath then
+        return file_operations.removeFile(lockPath)
+    else
+        return false
+    end
+end
+
+--Funnction that creates a lock for the directory at
+--the given path.
+function file_operations.lockDir(path)
+    local lockPath = getLockPath(path)
+    if lockPath then
+        return file_operations.createDir(lockPath)
+    else
+        return false
+    end
+end
+
+--Function that removes a lock for the directory at
+--the given path.
+function file_operations.unlockDir(path)
+    local lockPath = getLockPath(path)
+    if lockPath then
+        return file_operations.removeDir(lockPath)
+    else
+        return false
+    end
+end
+
 return file_operations
