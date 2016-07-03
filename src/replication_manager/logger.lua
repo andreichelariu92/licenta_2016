11292cf6ad1dc8ce71532f472dae5812dd643628
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-07-03 14:28:31 +0300

    Module that adds logging facility

---
+++ src/replication_manager/logger.lua
@@ -0,0 +1,45 @@
+--Table that will hold all the variables and functions
+--of the module. At the end of the script, it will be
+--returned to the rest of the application.
+local logger = {}
+logger.file = nil
+
+function logger.init(logFile)
+    if not logFile then
+        logFile = "logfile.lua"
+    end
+    
+    logger.file = assert(io.open(logFile, "a+"))
+end
+
+local function getInfo(level)
+    local info = debug.getinfo(level, "nl")
+    if info then
+        return info.name, info.currentline
+    end
+end
+
+function logger.log(message)
+    print("logger::Debug\n")
+    if not message then
+        --do nothing
+        return
+    end
+    
+    local name, line = getInfo(3)
+    if not name then
+        name = "global scope"
+    end
+
+    local logMessage = 
+        string.format("%s : %d  %s\n", name, line, message)
+    logger.file:write(logMessage)
+    logger.file:flush()
+end
+
+local function testFunc()
+    logger.log("Mesaj de test")
+end
+
+return logger
+
