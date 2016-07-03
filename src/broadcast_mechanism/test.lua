09fae018b3122e65ce04e814b6b481849e818fde
Andrei Chelariu <andreichelariu92@yahoo.com>
2016-07-03 14:18:30 +0300

    Use the test script to figure out the stack smashing problem

--- src/broadcast_mechanism/test.lua
+++ src/broadcast_mechanism/test.lua
@@ -1,20 +1,16 @@
 require "broadcast_mechanism_client"
 badParam = {}
-bcast_client.init(54321)
+bcast_client.init(12345)
 --wait
-local expectedClocks = 10
+local expectedClocks = 30
 local start = os.clock()
 while os.clock() - start < expectedClocks do
 end
-bcast_client.addConnection("127.0.0.1", 12345, "connection1")
+--bcast_client.addConnection("127.0.0.1", 12345, "connection1")
 
-messages = bcast_client.receive(5000)
-for messageIdx, message in pairs(messages) do
-    print(messageIdx)
-    print(message.id)
-    print(message.buffer)
+for messageCount = 1, 10, 1 do
+    bcast_client.send({buffer = "ceva\n", id = "id"})
 end
-
 messages = bcast.deinit()
 for messageIdx, message in pairs(messages) do
     print(messageIdx)
